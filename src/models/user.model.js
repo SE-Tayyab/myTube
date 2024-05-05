8f07b1cee20a9a5f95f7c5695db498309e1af80e
tayyab_sattar <tayyabsattar100@gmail.com>
2024-05-05 11:44:31 -0700

    userModel and videoModel

---
+++ src/models/user.model.js
@@ -0,0 +1,87 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      index: true,
+      lowecase: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      require: true,
+      index: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Videos",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    "TAYYAB SATTAR ARAIN",
+    {
+      expiresIn: "1d",
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    "TAYYAB SATTAR ARAIN",
+    {
+      expiresIn: "1d",
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
