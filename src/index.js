2e58f7a48d7fa47e1d4295af79e1f84852048e33
tayyab_sattar <tayyabsattar100@gmail.com>
2024-05-07 10:53:59 -0700

    app name changed to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
-import { app } from "./app.js"; // Import app from app.js
+import { app } from "./App.js"; // Import app from app.js
 
 dotenv.config({
   path: "./.env",
